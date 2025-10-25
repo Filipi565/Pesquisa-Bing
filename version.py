e4f4edaacd7d47bd72c59c88501a6f8e7f896546
Filipi565 <filipi565@hotmail.com>
2025-10-25 17:44:00 -0300

    Add repr and str methods

--- version.py
+++ version.py
@@ -2,6 +2,12 @@ class Version(int):
     def __new__(cls, major: int, minor: int, patch: int):
         return super().__new__(cls, (major << 16) | (minor << 8) | patch)
     
+    def __repr__(self) -> str:
+        return f"Version(major={self.major}, minor={self.minor}, patch={self.patch})"
+    
+    def __str__(self) -> str:
+        return f"{self.major}.{self.minor}.{self.patch}"
+    
     @property
     def major(self) -> int:
         return (self >> 16) & 0xff
@@ -14,4 +20,4 @@ class Version(int):
     def patch(self) -> int:
         return self & 0xff
     
-version = Version(1, 0, 0)
+version = Version(1, 1, 0)
