652ca7ea7105ec5da9bb6c67eb8c12ea26fbaf9a
Filipi565 <filipi565@hotmail.com>
2025-10-26 09:57:37 -0300

    update lang.pyi

--- lang.pyi
+++ lang.pyi
@@ -2,12 +2,12 @@ from tkinter import StringVar
 import util
 
 class Lang(util.StrEnum):
-    Title = ""
-    ErrorTitle = ""
-    ErrorMessage = ""
-    LevelName = ""
-    Start = ""
-    InvalidLevelError = ""
+    Title: str
+    ErrorTitle: str
+    ErrorMessage: str
+    LevelName: str
+    Start: str
+    InvalidLevelError: str
 
     @staticmethod
     def get_level_by_var(level_var: StringVar) -> int: ...
