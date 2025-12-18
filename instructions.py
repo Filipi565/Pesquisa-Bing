0764f8a15e344ba9d4e229a21f40b877c3c199d7
Filipi565 <filipi565@hotmail.com>
2025-12-18 15:34:46 -0300

    Update instructions

--- instructions.py
+++ instructions.py
@@ -1,10 +1,14 @@
 from zipfile import ZipFile
 from version import Version
+import shutil
 import os
 
 lib_folder: str
 zip_file:   ZipFile
 version:    Version
+
+if (version <= Version(1, 5, 1)): # type: ignore
+    shutil.rmtree(os.path.join(lib_folder, "util")) # type: ignore
 
 ZipFile.extractall(zip_file, lib_folder) # type: ignore
 os.remove(os.path.join(lib_folder, "instructions.py")) # type: ignore
